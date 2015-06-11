044940836e9ba904ef45e72576ad7f76b5824481
minhonline <minhonline12>
2015-06-11 13:45:36 +0700

    completed 30th course

--- src/main/java/cz/jiripinkas/jba/controller/UserController.java
+++ src/main/java/cz/jiripinkas/jba/controller/UserController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import cz.jiripinkas.jba.entity.Blog;
 import cz.jiripinkas.jba.entity.User;
+import cz.jiripinkas.jba.service.BlogService;
 import cz.jiripinkas.jba.service.UserService;
 
 @Controller
@@ -18,39 +20,53 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-	
+
+	@Autowired
+	private BlogService blogService;
+
 	@ModelAttribute("user")
-	public User construct(){
+	public User constructUser() {
 		return new User();
 	}
-	
+
+	@ModelAttribute("blog")
+	public Blog constructBlog() {
+		return new Blog();
+	}
+
 	@RequestMapping("/users")
-	public String users(Model model){
+	public String users(Model model) {
 		model.addAttribute("users", userService.findAll());
 		return "users";
 	}
-	
+
 	@RequestMapping("/user/{id}")
-	public String detail(Model model, @PathVariable int id){
+	public String detail(Model model, @PathVariable int id) {
 		model.addAttribute("user", userService.findOneWithBlog(id));
 		return "user-detail";
 	}
-	
+
 	@RequestMapping("/register")
-	public String showRegister(){
+	public String showRegister() {
 		return "user-register";
 	}
-	
-	@RequestMapping(value="/register", method = RequestMethod.POST)
-	public String doRegister(@ModelAttribute("user") User user){
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String doRegister(@ModelAttribute("user") User user) {
 		userService.save(user);
 		return "redirect:/register.html?success=true";
 	}
-	
+
 	@RequestMapping("/account")
-	public String account(Model model, Principal principal){
+	public String account(Model model, Principal principal) {
 		String userName = principal.getName();
 		model.addAttribute("user", userService.findOneWithName(userName));
 		return "account-detail";
+	}
+
+	@RequestMapping(value = "/account", method = RequestMethod.POST)
+	public String doBlog(@ModelAttribute("blog") Blog blog, Principal principal ) {
+		blogService.save(blog, principal.getName());
+		return "redirect:/account.html";
 	}
 }
